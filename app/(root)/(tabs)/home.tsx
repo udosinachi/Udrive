9a26100e62a15e33016bec80df584f1b292f6770
Udosinachi <62754095+udosinachi@users.noreply.github.com>
2024-11-01 00:26:54 +0100

    Auth page setup

---
+++ app/(root)/(tabs)/home.tsx
@@ -0,0 +1,16 @@
+import { SignedIn, SignedOut, useUser } from "@clerk/clerk-expo";
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Page() {
+  const { user } = useUser();
+
+  return (
+    <SafeAreaView>
+      <SignedIn>
+        <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
+      </SignedIn>
+    </SafeAreaView>
+  );
+}
